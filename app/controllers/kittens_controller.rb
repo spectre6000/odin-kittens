b36085676e959304ca2669d4a280cdd0e7031850
spectre6000 <spectre6000@gmail.com>
2015-05-28 15:18:34 -0600

    Kitten model and controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,21 @@
+class KittensController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end

a19d4849640d44229e45948f8096c1bc2482ae89
spectre6000 <spectre6000@gmail.com>
2015-05-28 16:18:12 -0600

    Controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,21 +1,49 @@
 class KittensController < ApplicationController
 
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      render 'show'
+    else
+      render 'new'
+    end
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      render 'show'
+    else
+      render 'new'
+    end
   end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 
 end

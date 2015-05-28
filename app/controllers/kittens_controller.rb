780882ee76ee6ffd80e6d949ceb948c2d2c9b503
spectre6000 <spectre6000@gmail.com>
2015-05-28 16:23:43 -0600

    Flash

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -19,7 +19,8 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
-      render 'show'
+      flash[:success] = "kitten created"
+      redirect_to root_path
     else
       render 'new'
     end
@@ -28,6 +29,7 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
+      flash[:success] = "kitten edited"
       render 'show'
     else
       render 'new'
@@ -37,6 +39,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
+    flash[:success] = "kitten killed"
     redirect_to root_url
   end
 
